fe6e0d751f1c0805058186db27088f9c045717d6
Ryan Koodish <Ryan.koodish@scsstudent.org>
2026-02-19 10:08:18 -0800

    feaT: complete video 3

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { theme } from "../theme";
+import React from "react";
 
 export default function App() {
   return (
@@ -14,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: theme.colorWhite,
     alignItems: "center",
     justifyContent: "center",
   },
