e1192b582c6739f94859e4e02aff39c7c3159d11
Ryan Koodish <Ryan.koodish@scsstudent.org>
2026-02-23 22:36:55 -0800

    feat: complete video 9

--- app/(tabs)/onboarding.tsx
+++ app/(tabs)/onboarding.tsx
@@ -1,10 +1,11 @@
 import { PlantlyButton } from "../../components/PlantlyButton";
+import { PlantlyImage } from "@/components/PlantlyImage";
 import { useUserStore } from "../../store/userStore";
 import { theme } from "../../theme";
 import { LinearGradient } from "expo-linear-gradient";
 import { useRouter } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function OnboardingScreen() {
     const router = useRouter();
@@ -21,6 +22,14 @@ export default function OnboardingScreen() {
             style={styles.container}
         >
             <StatusBar style="light" />
+            <View>
+                <Text style={styles.heading}>Plantly</Text>
+                <Text style={styles.tagline}>
+                    Keep your plants healthy and hydrated
+                </Text>
+            </View>
+
+            <PlantlyImage />
             <PlantlyButton title="Let Me In!!" onPress={handlePress} />
         </LinearGradient>
     );
@@ -29,11 +38,20 @@ export default function OnboardingScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
+        justifyContent: "space-evenly",
         alignItems: "center",
         backgroundColor: theme.colorWhite,
     },
-    text: {
+    heading: {
+        fontSize: 42,
+        color: theme.colorWhite,
+        fontWeight: "bold",
+        marginBottom: 12,
+        textAlign: "center",
+    },
+    tagline: {
         fontSize: 24,
+        color: theme.colorWhite,
+        textAlign: "center",
     },
 });
