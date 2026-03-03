915b7c1ca3baa71cd7ba5122b33990a58d77e1e7
Ryan Koodish <Ryan.koodish@scsstudent.org>
2026-03-03 09:58:34 -0800

    feaT: complete video 21

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,26 @@
 import React from "react";
-import { SplashScreen, Stack } from "expo-router";
+import { Stack } from "expo-router";
+import { useEffect } from "react";
+import * as QuickActions from "expo-quick-actions";
+import { Platform } from "react-native";
+import { useQuickActionRouting } from "expo-quick-actions/router";
 
-SplashScreen.preventAutoHideAsync();
 
 export default function Layout() {
+  useQuickActionRouting();
+
+  useEffect(() => {
+    QuickActions.setItems([
+      {
+        title: "Add Plant",
+        icon: Platform.OS === "ios" ? "symbol:leaf" : "leaf",
+        id: "0",
+        params: { href: "/new" },
+      },
+    ]);
+  }, []);
+
+
   return (
     <Stack>
       <Stack.Screen
